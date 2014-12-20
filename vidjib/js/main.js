f8b0b5da8c4c14d1b801cca2e6128f44d1c12bdd
Alex Zai <azai91@gmail.com>
2014-12-19 17:14:35 -0800

    (feat) able to connect two video stream with localhost

--- vidjib/js/main.js
+++ vidjib/js/main.js
@@ -26,7 +26,6 @@ navigator.getUserMedia = navigator.getUserMedia ||
 
 var localStream;
 var localPeerConnection;
-var remotePeerConnection;
 
 var localVideo = $('#localVideo').get(0);
 var remoteVideo = $('#remoteVideo').get(0);
@@ -56,6 +55,7 @@ function errorCallback (error) {
 
 function gotStreamSuccess (stream) {
   console.log("got local stream");
+  console.log(stream);
   localVideo.src = URL.createObjectURL(stream);
   localStream = stream;
   callButton.disabled = false;
@@ -71,7 +71,7 @@ function call () {
   callButton.disabled = true;
   hangupButton.disabled = false;
   console.log('starting call');
-  //check presence of local video and audio 
+  //check presence of local video and audio
   if (localStream.getVideoTracks().length > 0) {
     console.log('Using video device: ' + localStream.getVideoTracks()[0].label);
   }
@@ -79,63 +79,96 @@ function call () {
     console.log('Using audio device: ' + localStream.getAudioTracks()[0].label);
   }
 
+  //caller sets their peer connection
+  createPeerConnection();
+
+  localPeerConnection.addStream(localStream);
+  console.log("Added localStream to localPeerConnection");
+  localPeerConnection.createOffer(gotLocalDescriptionBeforeOffer,handleError);
+
+}
+
+function createPeerConnection() {
   var servers = null;
 
   localPeerConnection = new RTCPeerConnection(servers);
   console.log("Created local peer connection object localPeerConnection");
-  localPeerConnection.onicecandidate = gotLocalIceCandidate;
-
-  remotePeerConnection = new RTCPeerConnection(servers);
-  console.log("Created remote peer connection object remotePeerConnection");
-  remotePeerConnection.onicecandidate = gotRemoteIceCandidate;
-  remotePeerConnection.onaddstream = gotRemoteStream;
-
-  localPeerConnection.addStream(localStream);
-  console.log("Added localStream to localPeerConnection");
-  localPeerConnection.createOffer(gotLocalDescription,handleError);
+  localPeerConnection.onicecandidate = handleIceCandidate;
+  localPeerConnection.onaddstream = gotRemoteStream;
 }
 
-function gotLocalDescription(description){
-  localPeerConnection.setLocalDescription(description);
+function gotLocalDescriptionBeforeOffer(description){
   console.log("Offer from localPeerConnection: \n" + description.sdp);
-  remotePeerConnection.setRemoteDescription(description);
-  remotePeerConnection.createAnswer(gotRemoteDescription,handleError);
+  localPeerConnection.setLocalDescription(description);
+
+  //send offer to other
+  console.log('making offer');
+  socket.emit('offer', description);
 }
 
-function gotRemoteDescription(description){
-  remotePeerConnection.setLocalDescription(description);
-  console.log("Answer from remotePeerConnection: \n" + description.sdp);
-  localPeerConnection.setRemoteDescription(description);
+function gotLocalDescriptionBeforeAnswer(description){
+  localPeerConnection.setLocalDescription(description);
+  console.log("Answer from localPeerConnection: \n" + description.sdp);
+
+  //send offer to other
+  console.log('answering call');
+  socket.emit('answer', description);
 }
+
+//other client listening for offer and setting their remote description
+socket.on('offer', function(description) {
+  console.log('receiving offer');
+  createPeerConnection();
+  localPeerConnection.addStream(localStream);
+  localPeerConnection.setRemoteDescription(new RTCSessionDescription(description));
+  localPeerConnection.createAnswer(gotLocalDescriptionBeforeAnswer,handleError);
+});
+
+socket.on('answer', function(description) {
+  console.log('receiving answer');
+  localPeerConnection.setRemoteDescription(new RTCSessionDescription(description));
+});
+
+socket.on('candidate', function(candidate) {
+  console.log('receiving candidate info');
+  var candidate = new RTCIceCandidate({
+    sdpMLineIndex: candidate.label,
+    candidate: candidate.candidate
+  });
+
+  localPeerConnection.addIceCandidate(candidate);
+});
 
 function hangup() {
   console.log("Ending call");
   localPeerConnection.close();
-  remotePeerConnection.close();
   localPeerConnection = null;
-  remotePeerConnection = null;
   hangupButton.disabled = true;
   callButton.disabled = false;
 }
 
 function gotRemoteStream(event){
+  console.log();
+  console.log('remote stream');
+  console.log(event.stream);
   remoteVideo.src = URL.createObjectURL(event.stream);
   console.log("Received remote stream");
 }
 
-function gotLocalIceCandidate(event){
+function handleIceCandidate(event) {
   if (event.candidate) {
-    remotePeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
-    console.log("Local ICE candidate: \n" + event.candidate.candidate);
-  }
-}
+    var candidate = {
+      type: 'candidate',
+      label: event.candidate.sdpMLineIndex,
+      id: event.candidate.sdpMid,
+      candidate: event.candidate.candidate
+    };
 
-function gotRemoteIceCandidate(event){
-  if (event.candidate) {
-    localPeerConnection.addIceCandidate(new RTCIceCandidate(event.candidate));
-    console.log("Remote ICE candidate: \n " + event.candidate.candidate);
+    console.log('sending candidate info');
+    socket.emit('candidate', candidate);
   }
-}
+
+};
 
 function handleError(){}
 
--- vidjib/server.js
+++ vidjib/server.js
@@ -4,17 +4,35 @@ var server = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(server);
 
-// io.on('connection', function(socket){
-//   console.log('connection opened');
-//   socket.on('disconnect', function(){
-//     console.log('user disconnected');
-//   });
-//   socket.on('message', function(msg){
-//     console.log('msg is: ', msg);
-//     io.emit('msgtodisplay', msg);
-//   });
-// })
+io.on('connection', function(socket){
 
-app.use(express.static(path.join(__dirname, ".")));
+  // socket.on('disconnect', function(){
+  //   console.log('user disconnected');
+  // });
+
+  // socket.on('message', function(msg){
+  //   console.log('msg is: ', msg);
+  //   io.emit('msgtodisplay', msg);
+  // });
+  //
+
+  socket.on('offer', function(description) {
+    console.log('offer', description);
+    socket.broadcast.emit('offer', description); //change to broadcast
+  });
+
+  socket.on('answer', function(description) {
+    console.log('answer', description);
+    socket.broadcast.emit('answer', description);
+  });
+
+  socket.on('candidate', function(candidate) {
+    console.log('candidate', candidate);
+    socket.broadcast.emit('candidate', candidate);
+  });
+
+})
+
+app.use('/', express.static(path.join(__dirname, ".")));
 
 server.listen(3000);
